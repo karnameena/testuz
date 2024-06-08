0767f183fceedd39b09ad0140b6b87bd609d136f
karnameena <154045441+karnameena@users.noreply.github.com>
2024-06-08 15:36:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-import express from 'express';
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, Vercel with TypeScript!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const express = require("express"); const app = express(); app.get("/", (req, res) => { res.send("Express on Vercel"); }); const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
