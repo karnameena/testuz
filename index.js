dc2ccdfd8cbf3cf2a126a83b2bbd9d1ffaa24a1a
karnameena <154045441+karnameena@users.noreply.github.com>
2024-06-08 15:57:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const express = require("express"); const app = express(); app.get("/", (req, res) => { res.send("Express on Vercel"); }); const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+const express = require("express"); 
+const app = express();
+
+
+
+ app.get("/", (req, res) => { res.send('<h1 align="center">Karna Server Activated GK</h1>'); }); 
+ const PORT = process.env.PORT || 5000; 
+ app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
